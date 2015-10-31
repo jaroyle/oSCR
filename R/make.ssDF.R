cea6c9f3ac8235e039f749047ab541e5e9766c34
chrissuthy <chrissuthy@gmail.com>
2015-10-31 11:56:33 -0400

    added 2 functions. subFrame.R and make.ssDF.R

---
+++ R/make.ssDF.R
@@ -0,0 +1,29 @@
+
+################################################################################
+## A function that creates an state-space object of class 'ssDF'
+
+  make.ssDF <- function(scrFrame,buffer,res,cont.cov=F, fact.cov=F){
+                    ssDF <- list()
+                   for(i in 1:length(scrFrame$traps)){
+                     trpls <- scrFrame$traps[[i]]
+                     bl <- apply(trpls,2,min)
+                     tr <- apply(trpls,2,max)
+                     sxy <- expand.grid(seq(bl[1]-buffer,tr[1]+buffer,res),
+                                        seq(bl[2]-buffer,tr[2]+buffer,res))
+                     dd <- apply(e2dist(sxy,trpls),1,min)
+                     ssDF[[i]] <- sxy[dd<=buffer,]
+                     colnames(ssDF[[i]]) <- c("X","Y")
+                    if(cont.cov==T)
+                      ssDF[[i]]$cont <- runif(nrow(ssDF[[i]]), -1, 1)
+                    if(fact.cov==T){
+                      cat <- c("good","bad")
+                      prob <- c(0.6,0.4)
+                      tmp <- sample(cat,nrow(ssDF[[i]]),replace=T, prob=prob)
+                      ssDF[[i]]$fact <- factor(tmp)
+                    }
+                   }
+                    class(ssDF) <- "ssDF"
+                    return(ssDF)
+                  }
+
+################################################################################
---
+++ R/subFrame.r
@@ -0,0 +1,31 @@
+
+################################################################################
+## A function that sub sets an scrFrame object
+
+ subFrame <- function(scrFrame,subs){
+
+    nms <- names(scrFrame)
+    nms <- setdiff(nms,c("nocc","type"))
+    miss.nms <- setdiff(c("caphist","traps","trapCovs","indCovs","trapOperation"),nms)
+    scrFrame.sub <- list()
+    for(i in nms){
+      scrFrame.sub[[i]] <- list()
+     if(is.null(scrFrame[[i]])){
+       scrFrame.sub[[i]] <- NULL
+     }else{
+      for(j in 1:length(subs)){
+        scrFrame.sub[[i]][[j]] <- scrFrame[[i]][[subs[j]]]
+      }
+     }
+    }
+    for(i in miss.nms){
+      scrFrame.sub[[i]] <- NULL
+    }
+    new.nocc <- max(unlist(lapply(scrFrame.sum$caphist,function(x) dim(x)[3])))
+    scrFrame.sub$nocc <- scrFrame$nocc
+    scrFrame.sub$type <- scrFrame$type
+    class(scrFrame.sub) <- "scrFrame"
+    return(scrFrame.sub)
+ }
+
+################################################################################

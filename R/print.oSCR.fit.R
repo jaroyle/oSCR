7970e0f8858cc9fcf3a10896f96d478a202105ec
chrissuthy <chrissuthy@gmail.com>
2016-01-05 12:14:46 -0500

    fixed edited print function

--- R/print.oSCR.fit.R
+++ R/print.oSCR.fit.R
@@ -2,7 +2,7 @@ print.oSCR.fit <-
 function(x, burn=NULL, ...) {
 mod <- x$call[["model"]]
 cat(" Model: ", paste(mod)[-1],fill=TRUE)
-cat(" Run time: ", x$proctime," minutes",fill=TRUE)
+cat(" Run time: ", x$proctime," seconds",fill=TRUE)
 cat(" ",fill=TRUE)
 cat("-------------------------------------------------",fill=TRUE)
 cat(" Summary table of model parameters:","\n")

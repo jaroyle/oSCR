9a2fec6e6ff3efe1b81ac9393f965c08d307db7f
danlinden <danlinden@gmail.com>
2016-03-10 12:27:05 -0500

    make predict.oSCR use all arguments from model fitting call

--- R/predict.oSCR.R
+++ R/predict.oSCR.R
@@ -1,13 +1,18 @@
 predict.oSCR <-
-function(scrFrame, ssDF, scr.fit){
+function(scrFrame, scr.fit, ssDF, costDF=NULL){
  
-  mles <- scr.fit$rawOutput$estimates
-  mod <- scr.fit$call
-  mc<- mod$multicatch
-  tr<- mod$trimS
-  prediction <- oSCR.fit(scrFrame=scrFrame, ssDF=ssDF, model=mod, start.vals=mles, trimS=tr, 
-  multicatch=scr.fit$call$mc,
-  predict=TRUE)
+  mles <- scr.fit$rawOutput$estimate
+  call <- scr.fit$call
+  
+  oSCR.fit2 <- oSCR.fit
+  # modify the arguments to match the original call (skip the data)
+  call.fix <- names(call)[!names(call) %in% c("","scrFrame","ssDF","costDF")]
+  for(i in 1:length(call.fix)){
+    formals(oSCR.fit2)[[call.fix[i]]]<- call[[call.fix[i]]]
+  }
+  prediction <- oSCR.fit2(scrFrame=scrFrame, ssDF=ssDF, costDF=costDF,
+                          #critical arguments that differ for prediction
+                          start.vals=mles, predict=TRUE)
   return(prediction)
  
 }

3f5c578402aa14490a9b2e3023174ee38f783d13
chrissuthy <chrissuthy@gmail.com>
2016-06-05 14:41:50 -0400

    Updated to match make.scrFrame() function

--- R/subFrame.r
+++ R/subFrame.r
@@ -3,29 +3,51 @@
 ## A function that sub sets an scrFrame object
 
  subFrame <- function(scrFrame,subs){
-
-    nms <- names(scrFrame)
-    nms <- setdiff(nms,c("nocc","type"))
-    miss.nms <- setdiff(c("caphist","traps","trapCovs","indCovs","trapOperation"),nms)
-    scrFrame.sub <- list()
-    for(i in nms){
-      scrFrame.sub[[i]] <- list()
-     if(is.null(scrFrame[[i]])){
-       scrFrame.sub[[i]] <- NULL
-     }else{
-      for(j in 1:length(subs)){
-        scrFrame.sub[[i]][[j]] <- scrFrame[[i]][[subs[j]]]
-      }
+   
+   caphist <- scrFrame$caphist[subs]
+   traps <- scrFrame$traps
+   if(!is.null(scrFrame$indCovs)){
+     indCovs <- scrFrame$indCovs[subs]
+   }else{
+     indCovs <- scrFrame$indCovs[subs]
+   }
+   if(!is.null(scrFrame$trapCovs)){
+     trapCovs <- scrFrame$trapCovs[subs]
+   }else{
+     trapCovs <- scrFrame$trapCovs[subs]
+   }
+   if(!is.null(scrFrame$trapOperation)){
+     trapOperation <- scrFrame$trapOperation[subs]
+   }else{
+     trapOperation <- scrFrame$trapOperation[subs]
+   }
+   
+   caphist.dimensions <- sapply(caphist,dim)
+   
+   type <- scrFrame$type
+   
+   #mean maximum distance moved
+   max.dist <- NULL
+   for (i in 1:length(caphist)) {
+     for (j in 1:nrow(caphist[[i]])) {
+       where <- apply(caphist[[i]][j, , ], 1, sum) > 0
+       if (sum(where) > 1)
+         max.dist <- c(max.dist, max(0, dist(traps[[i]][where, c("X", "Y")]), na.rm = T))
      }
-    }
-    for(i in miss.nms){
-      scrFrame.sub[[i]] <- NULL
-    }
-    new.nocc <- max(unlist(lapply(scrFrame.sub$caphist,function(x) dim(x)[3])))
-    scrFrame.sub$nocc <- scrFrame$nocc
-    scrFrame.sub$type <- scrFrame$type
-    class(scrFrame.sub) <- "scrFrame"
-    return(scrFrame.sub)
+   }
+   mmdm <- mean(max.dist[max.dist > 0], na.rm = T)
+   
+   scrFrame <- list("caphist" = caphist,
+                    "traps" = traps,
+                    "indCovs" = indCovs,
+                    "trapCovs" = trapCovs,
+                    "trapOperation" = trapOperation,
+                    "occasions" = caphist.dimensions[3,],
+                    "type" = type,
+                    "mmdm" = mmdm)
+   
+   class(scrFrame) <- "scrFrame"  
+   return(scrFrame)
  }
 
 ################################################################################

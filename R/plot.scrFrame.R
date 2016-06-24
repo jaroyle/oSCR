d3dd74afee9426723dd1c3f2c26c646b09fe3a0b
chrissuthy <chrissuthy@gmail.com>
2016-06-24 13:21:26 -0400

    updated plotting function

--- R/plot.scrFrame.R
+++ R/plot.scrFrame.R
@@ -12,22 +12,29 @@ plot.scrFrame<-function(scrFrame, ask=TRUE){
     tmp.tr <- scrFrame$traps[[s]]
     
     for(i in 1:nrow(tmp.ch)){
-      pick <- apply(apply(tmp.ch,c(1,2),sum)>1,2,sum)  
+      pick <- apply(tmp.ch[i,,]>0,1,sum)  
       s.ind.xy <- rbind(s.ind.xy, tmp.tr[rep(1:nrow(tmp.tr),pick),c("X","Y")])
       s.ind <- c(s.ind,rep(i,sum(pick)))
       
-      }
+    }
     all.ind.xy[[s]] <- data.frame(ind = s.ind, 
                                   x   = s.ind.xy[,1], 
                                   y   = s.ind.xy[,2])
     mu.x[[s]] <- tapply(all.ind.xy[[s]]$x,all.ind.xy[[s]]$ind,mean)
     mu.y[[s]] <- tapply(all.ind.xy[[s]]$y,all.ind.xy[[s]]$ind,mean)
   }
+  par(oma=c(0,0,0,0))
   for(s in 1:length(scrFrame$caphist)){
-    plot(scrFrame$traps[[s]][,c("X","Y")])
+    plot(scrFrame$traps[[s]][,c("X","Y")], asp=1, type="n",las=1)
+    clr <- sample(colors(),nrow(tmp.ch))
     for(i in 1:nrow(tmp.ch)){
-      segments(mu.x[[s]][i],mu.x[[s]][i], 
-               all.ind.xy[[s]]$x, all.ind.xy[[s]]$y)
+      to.x <- all.ind.xy[[s]]$x[all.ind.xy[[s]]$ind %in% i]
+      to.y <- all.ind.xy[[s]]$y[all.ind.xy[[s]]$ind %in% i]
+      segments(rep(mu.x[[s]][i],length(to.x)),
+               rep(mu.y[[s]][i],length(to.y)), 
+               to.x, to.y, col=clr[i], lwd=2)
     }
+    points(scrFrame$traps[[s]][,c("X","Y")], pch="+",cex=1)
+    points(mu.x[[s]],mu.y[[s]],pch=16,cex=1.5,col=clr)
   }  
 }

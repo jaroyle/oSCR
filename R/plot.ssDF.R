df79e2db75e53650c6ae476c0ebc6221887d196f
chrissuthy <chrissuthy@gmail.com>
2016-06-23 23:22:57 -0400

    tidying up

--- R/plot.ssDF.R
+++ R/plot.ssDF.R
@@ -1,9 +1,9 @@
 plot.ssDF <- function(ssDF, scrFrame=NULL, collapse=FALSE){
-  
+  par(oma=c(0,0,0,0),mar=c(1,1,1,1))
   if(!collapse){
   for(i in 1:length(ssDF)){
     ss.xy <- ssDF[[i]][,c("X","Y")]
-    plot(ss.xy, pch=16, cex=0.5, col="grey", asp = TRUE)
+    plot(ss.xy, pch=16, cex=0.5, col="grey", asp = TRUE,axes=FALSE)
     if(!is.null(scrFrame)){
       sf.xy <- scrFrame$traps[[i]][,c("X","Y")]  
       points(sf.xy, pch=15, col=4)
@@ -18,8 +18,9 @@ plot.ssDF <- function(ssDF, scrFrame=NULL, collapse=FALSE){
         sf.xy <- rbind(sf.xy, scrFrame$traps[[i]][,c("X","Y")])  
       }
   }
-  plot(ss.xy, pch=16, cex=0.5, col="grey", asp = TRUE)
+  plot(ss.xy, pch=16, cex=0.5, col="grey", asp = TRUE, axes=FALSE)
   if(!is.null(scrFrame))
      points(sf.xy, pch=15, col=4)
   }   
+  box(bty="o")
 }  

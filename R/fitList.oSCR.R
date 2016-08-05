367ce2379278912cd277ad82b8fe10493dc90442
chrissuthy <chrissuthy@gmail.com>
2016-08-05 11:39:40 -0400

    clean up

--- R/fitList.oSCR.R
+++ R/fitList.oSCR.R
@@ -7,34 +7,19 @@ fitList.oSCR <- function(x, rename=F, names = NULL, drop=NULL){
     fl.names <- names(x)
   
   if(rename==TRUE){
-#    n1 <- paste(lapply(x,function(z) z$call$model[2]))
-#    n1 <- gsub("\\(","",n1)
-#    n1 <- gsub("\\)","",n1)
-#    n1 <- gsub(" ","",n1)
     n1 <- paste(lapply(x,function(z) update.formula(formula(z$call$model[[2]]),NULL ~ .)))    
     n1 <- ifelse(n1 %in% "NULL",".",n1)
-#    n2 <- paste(lapply(x,function(z) z$call$model[3]))
-#    n2 <- gsub("\\(","",n2)
-#    n2 <- gsub("\\)","",n2)
-#    n2 <- gsub(" ","",n2)
     n2 <- paste(lapply(x,function(z) update.formula(formula(z$call$model[[3]]),NULL ~ .)))    
     n2 <- ifelse(n2 %in% "NULL",".",n2)
-#    n3 <- paste(lapply(x,function(z) z$call$model[4]))
-#    n3 <- gsub("\\(","",n3)
-#    n3 <- gsub("\\)","",n3)
-#    n3 <- gsub(" ","",n3)
     n3 <- paste(lapply(x,function(z) update.formula(formula(z$call$model[[4]]),NULL ~ .)))    
     n3 <- ifelse(n3 %in% "NULL",".",n3)
-#    n4 <- paste(lapply(x,function(z) z$call$model[5]))
-#    n4 <- gsub("\\(","",n4)
-#    n4 <- gsub("\\)","",n4)
-#    n4 <- gsub(" ","",n4)
+
     count.mods <- lapply(x, function(z)length(z$call$model))
-    if(any(count.mods>4)){
+    if(any(count.mods>3)){
       extract.fn <- function(z){
-        if(length(z$call$model)>4)
+        if(length(z$call$model)==5)
           tmp <- unlist(update.formula(formula(z$call$model[[5]]),NULL ~ .))
-        if(length(z$call$model)==4)
+        if(length(z$call$model)<5)
           tmp <- "."
         return(tmp)
       }

066fbff1ff349128ab562732c999b86a83668be6
unknown <jaroyle@gmail.com>
2016-08-19 22:22:49 -0400

    edited beardata names and help file

--- R/test.oSCR.R
+++ R/test.oSCR.R
@@ -9,7 +9,7 @@ test.oSCR <- function(test=c(1,2,3)[1]){
     data(rbs)
     rbs1.sf <- subFrame(rbs.sf,2)
     rbs1.ss <- make.ssDF(rbs1.sf,res=0.5,buffer=3)
-    rbs.test <- oSCR.fit(scrFrame=rbs1.sf,ssDF=rbs1.ss)
+    rbs.test <- oSCR.fit(scrFrame=rbs1.sf,ssDF=rbs1.ss, start.vals=c(-3.3, 0.03, -0.991))
   
     cat("Output table should look like this:\n")
   
@@ -29,7 +29,7 @@ test.oSCR <- function(test=c(1,2,3)[1]){
     cat("\n")
   
     data(ocelot)
-    ocelot.test <- oSCR.fit(scrFrame=ocelot.sf,ssDF=ocelot.ss)
+    ocelot.test <- oSCR.fit(scrFrame=ocelot.sf,ssDF=ocelot.ss, start.vals=c(-3.52, 6.40, -1.53, 0.0) )
   
     cat("Output table should look like this:\n")
 

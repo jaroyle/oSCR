f3574aca26cfef503dd19cff19100ae0c3ee9cb7
chrissuthy <chrissuthy@gmail.com>
2017-10-24 17:39:34 -0400

    edit helper functions (for generality)

--- R/oSCR.parfit.R
+++ R/oSCR.parfit.R
@@ -1,41 +1,42 @@
-oSCR.parfit <-
-function(mods, data, ncores=2){
-library(doParallel)
-
-sf<- data$sf
-ss<- data$ss
-cs<- data$cs
- 
-wrapperX<- function(model.number, mods, sf, ss, cs=NULL){
- 
-    mod <- list(mods[[1]][[model.number]], #density
-              mods[[2]][[model.number]], #detection
-              mods[[3]][[model.number]], #sigma
-              mods[[4]][[model.number]]) #asu
- 
-  fm <- oSCR.fit(scrFrame=sf, ssDF=ss, costDF=cs, model=mod, trimS=2.5,
-                 se=TRUE, distmet="euc",sexmod="constant")
-  save(fm, file=paste("model",model.number,".RData",sep=""))
-  return(fm)
-}
-nmods<- nrow(mods)
-cl<-makeCluster(ncores)  
-registerDoParallel(cl)
-out <-foreach(i=1:nmods) %dopar% {
-library(oSCR)
-tmp<- wrapperX(i, mods, data$sf, data$ssDF)
-  return(tmp)
-}
-stopCluster(cl)
-
-save("out",file="models.RData")
-
-tmp<- list()
-for(i in 1:length(out)){
-    tmp[[i]]<- out[[i]]
-    tmp[[i]]$call$model <- list("list",paste(mods[i,1]),paste(mods[i,2]),paste(mods[i,3]),paste(mods[i,4]))
-}
-out<- fitList.oSCR(tmp,rename=TRUE )
-return(out)
-
+oSCR.parfit <- function(mods, ncores=2,
+                        scrFrame, ssDF, encmod = c("B", "P", "CLOG")[1], multicatch = FALSE, theta = 2, 
+                        trimS = NULL, DorN = c("D", "N")[1], sexmod = c("constant", "session")[1], 
+                        costDF = NULL, distmet = c("euc", "user", "ecol")[1], directions = 8, 
+                        PROJ = NULL, rsfDF = NULL, RSF = FALSE, telemetry = c("none","ind","dep")[1],
+                        se = TRUE, predict = FALSE, start.vals = NULL, getStarts = FALSE, pxArea = 1, 
+                        plotit = F, mycex = 1, nlmgradtol = 1e-06, nlmstepmax = 10, smallslow = FALSE, 
+                        print.level = 0){
+  library(doParallel)
+  wd <- getwd()
+  par.wrap <- function(model.number, mods,
+                       scrFrame, ssDF, encmod, multicatch, theta, trimS, DorN, sexmod, 
+                       costDF, distmet, directions, PROJ, rsfDF, RSF, telemetry, se, predict, start.vals, 
+                       getStarts, pxArea, plotit, mycex, nlmgradtol, nlmstepmax, smallslow, print.level){
+    mod <- list(as.formula(paste(mods[[1]][[model.number]])), #density
+                as.formula(paste(mods[[2]][[model.number]])), #detection
+                as.formula(paste(mods[[3]][[model.number]])), #sigma
+                as.formula(paste(mods[[4]][[model.number]]))) #asu
+    
+    fm <- oSCR.fit(model = mod, scrFrame, ssDF, encmod, multicatch, theta, trimS, DorN, sexmod, 
+                   costDF, distmet, directions, PROJ, rsfDF, RSF, telemetry, se, predict, start.vals, 
+                   getStarts, pxArea, plotit, mycex, nlmgradtol, nlmstepmax, smallslow, print.level)
+    return(fm)
+  }
+  nmods <- nrow(mods)
+  cl <- makeCluster(ncores)  
+  registerDoParallel(cl)
+  out <- foreach(i = 1:nmods) %dopar% {
+    library(oSCR)
+    tmp <- par.wrap(i, mods, scrFrame, ssDF, encmod, multicatch, theta, 
+                    trimS, DorN, sexmod, costDF, distmet, directions, PROJ, 
+                    rsfDF, RSF, telemetry, se, predict, start.vals, getStarts, 
+                    pxArea, plotit, mycex, nlmgradtol, nlmstepmax, smallslow, 
+                    print.level)
+    save("tmp",file=paste0(paste0(wd,"/model_",i,".RData")))
+    return(tmp)
+  }
+  stopCluster(cl)
+  save("out",file="models.RData")
+  out<- fitList.oSCR(out,rename=TRUE)
+  return(out)
 }

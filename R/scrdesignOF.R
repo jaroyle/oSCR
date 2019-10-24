6ce84b5e5b41482fa32c5721cd0261f96a4ff874
chrissuthy <chrissuthy@gmail.com>
2019-10-24 14:54:31 -0400

    p2bar was computed wrong. Fixed.

--- R/scrdesignOF.R
+++ R/scrdesignOF.R
@@ -10,30 +10,32 @@ scrdesignOF <- function(v,          #subset/k index
   traps <- alltraps[v,]
   ntraps <- nrow(traps)
   p0 <- exp(beta0)
-  pmat <- p0*exp(-e2dist(traps, statespace)^2/(2*sigma*sigma))
+  dmat <- e2dist(traps, statespace)
+  pmat <- p0*exp(-dmat*dmat/(2*sigma*sigma))
 
   # crit 1: pbar
-  capt.pr <- 1-exp(-pmat)  
-  notcapt.pr <- 1-capt.pr
-  pbar <- mean(1 - exp(colSums(log(notcapt.pr))))
-  crit1 <- pbar
+  pcaptmat <- 1-exp(-pmat)  
+  pnotcaptmat <- 1-pcaptmat
+  pbar <- mean(1 - exp(colSums(log(pnotcaptmat))))
+  crit1 <- 1 - pbar
 
+  
   # crit 2: p2bar (1 - pr(no capts) - pr(capt in 1 trap))
-  capt.pr <- 1-exp(-pmat)  
-  notcapt.pr <- 1-capt.pr
-  pbar <- mean(1 - exp(colSums(log(notcapt.pr))))
-  p0times<- 1-pbar
-  cap.trap.j <- 1-(notcapt.pr)
-  bb <- cap.trap.j/(notcapt.pr)
-  notcap <- exp(colSums(log(notcapt.pr)))
-  bling <-  matrix(notcap, ncol=length(notcap), nrow=ntraps, byrow=TRUE)*bb
+  p0times <- 1-pbar
+  capattrapj <- pcaptmat
+  bb <- pcaptmat/(pnotcaptmat)
+  notcap <- exp(colSums(log(pnotcaptmat)))
+  bling <- matrix(notcap, ncol=length(notcap), nrow=ntraps, byrow=TRUE)*bb
   p1time <- colSums(bling)
-  crit2 <- mean(1 - p0times - p1time)
+  p2bar <- mean(1 - p0times - p1time)
+  crit2 <- 1-p2bar
 
   # crit 3: pbar + p2bar
-  crit3 <- crit1 + crit2 
+  p12bar <- pbar + p2bar
+  crit3 <- 1 - p12bar 
 
   # return both 1-pbar and 1-p2bar which we will minimize using SCRdesign
-  valueOF <- 1 - c(crit1, crit2, crit3)[crit]
+  valueOF <- c(crit1, crit2, crit3)[crit]
   valueOF
 }
+
